efabc587eeb289983e34132c05e50c5e395ba0e6
itsishant <modakmania@gmail.com>
2026-02-12 02:16:58 +0530

    updated with nativewind

--- App.jsx
+++ App.jsx
@@ -5,64 +5,24 @@ import {
   View,
   TextInput,
 } from 'react-native';
-
+import './global.css';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.content}>
-        <Text style={styles.text}>Simply Management</Text>
-        <View>
-          <TextInput style={styles.inputAccessoryView}></TextInput>
-        </View>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Create Task</Text>
-        </TouchableOpacity>
+    <SafeAreaView className="flex min-h-screen justify-center items-center bg-black">
+      <View className="flex-1 justify-between items-center py-10">
+        <Text className="text-5xl text-green-200  mt-6 font-bold text-center">
+          Silly
+          <Text className="text-5xl text-blue-600 font-bold text-center">
+            {' '}
+            Manage
+          </Text>
+        </Text>
       </View>
     </SafeAreaView>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-  },
-  content: {
-    flex: 0.5,
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingVertical: 40,
-  },
-  text: {
-    color: 'white',
-    fontSize: 30,
-    fontWeight: 'bold',
-  },
-  button: {
-    width: 160,
-    height: 50,
-    backgroundColor: 'blue',
-    borderRadius: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 16,
-    fontWeight: '600',
-  },
-  inputAccessoryView: {
-    backgroundColor: 'transparent',
-    padding: 10,
-    borderColor: "white",
-    color: "white",
-    borderWidth: 1,
-    borderRadius: 5,
-    width: 350
-  },
-});
